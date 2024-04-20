e65296fe0bd4b36803a0172fbbbd15bd80fdc0de
Aman007chahar <aman007chahar@gmail.com>
2024-04-20 18:33:33 +0530

    Project made using basic concepts of OOP and java.

---
+++ src/ATMmachineUsingOOP/ATM.java
@@ -0,0 +1,102 @@
+package ATMmachineUsingOOP;
+
+import java.util.Scanner;
+
+public class ATM {
+    float balance;
+    public int PIN = 1234;
+    Scanner scanner = new Scanner(System.in);
+
+    public void checkPIN() throws InterruptedException {
+
+        System.out.println("Enter your PIN : ");
+
+        int enteredPIN = scanner.nextInt();
+        if(PIN==enteredPIN){
+            menu();
+
+        }else{
+            System.out.println("Enter a valid PIN");
+            System.out.println();
+            System.out.println();
+        }
+    }
+
+    public void menu() throws InterruptedException {
+        System.out.println();
+        System.out.println();
+        System.out.println("Enter Your choice");
+        System.out.println("1. Check A/C Balance");
+        System.out.println("2. Withdraw Money");
+        System.out.println("3. Deposit Money");
+        System.out.println("4. EXIT");
+
+        int choice = scanner.nextInt();
+        switch (choice){
+            case 1:
+                checkBalance();
+                break;
+            case 2:
+                withdrawMoney();
+                break;
+            case 3:
+                depositMoney();
+                break;
+            case 4:
+                exit();
+                break;
+            default:
+                System.out.println("Enter valid choice");
+                System.out.println();
+                System.out.println();
+                menu();
+
+        }
+
+    }
+
+    public void checkBalance() throws InterruptedException {
+        System.out.println("Balance : " + balance);
+        System.out.println();
+        System.out.println();
+        menu();
+    }
+
+    public void withdrawMoney() throws InterruptedException {
+        System.out.println("Enter amount to withdraw : ");
+        int withdrawAmount = scanner.nextInt();
+        if(withdrawAmount > balance){
+            System.out.println("Insufficient balance.");
+        }else{
+            balance = balance - withdrawAmount;
+            System.out.println("Money withdrawl successful");
+            System.out.println("Balance after withdrawl :" + balance);
+        }
+        System.out.println();
+        System.out.println();
+        menu();
+    }
+
+    public void depositMoney() throws InterruptedException {
+        System.out.println("Enter amount to deposit : ");
+        int depositAmount = scanner.nextInt();
+        balance = balance + depositAmount;
+        System.out.println("Amount deposited successfully!!");
+        System.out.println("Balance after deposit : " + balance);
+        System.out.println();
+        System.out.println();
+        menu();
+    }
+
+    public void exit() throws InterruptedException{
+        System.out.print("Exiting System");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(".");
+            Thread.sleep(400);
+        }
+        System.out.println();
+        System.out.println("Thank You for using ATM");
+    }
+
+
+}
---
+++ src/ATMmachineUsingOOP/ATMMachine.java
@@ -0,0 +1,21 @@
+package ATMmachineUsingOOP;
+
+import java.util.Scanner;
+
+public class ATMMachine {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ATM atm = new ATM();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Your PIN : ");
+        int password  = scanner.nextInt();
+        if(password == atm.PIN){
+            atm.menu();
+        }
+
+    }
+}
+
+
